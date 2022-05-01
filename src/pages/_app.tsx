4dbc7a2fc668790330229bb15cc71446feafc7e3
Jyoti Nanda <j_k_nanda@hotmail.com>
2022-04-30 20:57:31 -0700

    adding routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
-import { AppProps } from "next/app";
+import { AppProps, NextWebVitalsMetric } from "next/app";
 import "@styles/normalize.css";
 import "@styles/layout.css";
 import "@styles/commons.css";
 import "@styles/disclaimer.css";
+
+export const reportWebVitals = (metric: NextWebVitalsMetric) => {
+  console.log(metric);
+}
 
 const App: React.FC<AppProps> = (props) => {
   const { Component, pageProps } = props;
